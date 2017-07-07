b3b47072684bb23d63235349475706377734d109
Manuel Cheta <chetamanuel@gmail.com>
2017-07-07 11:59:05 +0100

    Playing with sort-of-animations in Chrome's console
    Basically using console.log + CSS in order to display a floating
    message.

---
+++ console.log.animation.js
@@ -0,0 +1,29 @@
+let spacing = " ";
+let timeStep = 33;
+let runs = 150;
+
+function for1(){
+for (let i = 1; i < runs; i++) {
+    setTimeout(function(){  
+		//console.clear();
+        console.log("%s%c%s",spacing,"color: #0ff;background: #00f;padding: 3px;","[Mike]")      
+        spacing += " ";
+        }, timeStep*i); 
+}
+};
+
+
+
+function for2(){
+for (let j = 1; j < runs; j++) {
+    setTimeout(function(){  
+		//console.clear();
+        console.log("%s%c%s",spacing,"color: #0ff;background: #00f;padding: 3px;","[Mike]")      
+        spacing = spacing.substring(0, spacing.length - 1);
+        }, timeStep*j); 
+}
+};
+
+for1();
+setTimeout(for2,timeStep * runs);
+
