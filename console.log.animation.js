512fdee5da75da69bffb677a3592253e79c52069
Manuel Cheta <chetamanuel@gmail.com>
2017-07-07 12:00:19 +0100

    Ooops, we need to clear the console
    Done.

--- console.log.animation.js
+++ console.log.animation.js
@@ -5,7 +5,7 @@ let runs = 150;
 function for1(){
 for (let i = 1; i < runs; i++) {
     setTimeout(function(){  
-		//console.clear();
+		console.clear();
         console.log("%s%c%s",spacing,"color: #0ff;background: #00f;padding: 3px;","[Mike]")      
         spacing += " ";
         }, timeStep*i); 
@@ -17,7 +17,7 @@ for (let i = 1; i < runs; i++) {
 function for2(){
 for (let j = 1; j < runs; j++) {
     setTimeout(function(){  
-		//console.clear();
+		console.clear();
         console.log("%s%c%s",spacing,"color: #0ff;background: #00f;padding: 3px;","[Mike]")      
         spacing = spacing.substring(0, spacing.length - 1);
         }, timeStep*j); 
